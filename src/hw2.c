d728580a98a545a3632d1d033c41b700826a1aee
Ian Kaufman <Kaufmanian49@gmail.com>
2024-10-04 05:19:34 +0000

    try 6

--- src/hw2.c
+++ src/hw2.c
@@ -13,20 +13,51 @@ unsigned int extract_bits(unsigned int value, int start, int count)
 }
 void store_values(unsigned int packets[], char *memory) 
 {
-    int length = extract_bits(packets[0], 0, 8);  
-    int index = 0;
-
-    for (int i = 3; i < 3 + length; i++) 
+    int packet_index = 0;
+    while (true) 
     {
-        unsigned int data = packets[i];
+        int packet_type = extract_bits(packets[packet_index], 24, 8);
+        if (packet_type != 0x40) break; 
         
-        memory[index++] = (char)(data >> 24); 
-        memory[index++] = (char)(data >> 16);  
-        memory[index++] = (char)(data >> 8);   
-        memory[index++] = (char)(data);      
+        int length = extract_bits(packets[packet_index], 0, 8);
+        unsigned int address = (packets[packet_index + 2] & 0xFFFFFFFC); 
+        if (address >= 1 << 20) break; 
+
+        int first_be = extract_bits(packets[packet_index + 1], 24, 4);
+        int last_be = extract_bits(packets[packet_index + 1], 28, 4);
+
+        for (int i = 0; i < length; i++) 
+        {
+            unsigned int data = packets[packet_index + 3 + i];
+
+            int mem_index = address + (i * 4);
+            
+            if (i == 0) 
+            {
+                if (first_be & 1) memory[mem_index] = (char)(data & 0xFF);
+                if (first_be & 2) memory[mem_index + 1] = (char)((data >> 8) & 0xFF);
+                if (first_be & 4) memory[mem_index + 2] = (char)((data >> 16) & 0xFF);
+                if (first_be & 8) memory[mem_index + 3] = (char)((data >> 24) & 0xFF);
+            } 
+            else if (i == length - 1) 
+            {
+                if (last_be & 1) memory[mem_index] = (char)(data & 0xFF);
+                if (last_be & 2) memory[mem_index + 1] = (char)((data >> 8) & 0xFF);
+                if (last_be & 4) memory[mem_index + 2] = (char)((data >> 16) & 0xFF);
+                if (last_be & 8) memory[mem_index + 3] = (char)((data >> 24) & 0xFF);
+            } 
+            else 
+            {
+                memory[mem_index] = (char)(data & 0xFF);
+                memory[mem_index + 1] = (char)((data >> 8) & 0xFF);
+                memory[mem_index + 2] = (char)((data >> 16) & 0xFF);
+                memory[mem_index + 3] = (char)((data >> 24) & 0xFF);
+            }
+        }
+
+        packet_index += (3 + length);
     }
 }
-
 unsigned int* create_completion(unsigned int packets[], const char *memory)
 {
     (void)packets;
@@ -39,11 +70,13 @@ void print_packet(unsigned int packet[])
 {
     int packet_type = extract_bits(packet[0], 24, 8);
     int length = extract_bits(packet[0], 0, 8);
-    unsigned int address = packet[1];
+
+    unsigned int address = (packet[2] >> 2) & 0x3FFFFFFF;  
+
     int requester_id = extract_bits(packet[1], 0, 16);
     int tag = extract_bits(packet[1], 16, 8);
-    int last_be = extract_bits(packet[2], 28, 4);
-    int first_be = extract_bits(packet[2], 24, 4);
+    int last_be = extract_bits(packet[1], 28, 4); 
+    int first_be = extract_bits(packet[1], 24, 4); 
 
     if (packet_type == 0x40) 
     {
@@ -54,7 +87,7 @@ void print_packet(unsigned int packet[])
         printf("Packet Type: Read\n");
     }
 
-    printf("Address: %u\n", address);
+    printf("Address: %u\n", address); 
     printf("Length: %d\n", length);
     printf("Requester ID: %d\n", requester_id);
     printf("Tag: %d\n", tag);
@@ -66,7 +99,7 @@ void print_packet(unsigned int packet[])
         printf("Data: ");
         for (int i = 3; i < 3 + length; i++) 
         {
-            printf("%d ", packet[i]);
+            printf("%d ", (int)packet[i]);
         }
         printf("\n");
     } 
