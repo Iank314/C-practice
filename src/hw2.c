f5fb2ec28f3150b5b13d88d11e188ac52688877b
Ian Kaufman <Kaufmanian49@gmail.com>
2024-10-04 04:11:08 +0000

    try 4

--- src/hw2.c
+++ src/hw2.c
@@ -18,43 +18,42 @@ unsigned int* create_completion(unsigned int packets[], const char *memory)
 	return NULL;
 }
 
-unsigned int extract_bits(unsigned int value, int start, int count) 
-{
+unsigned int extract_bits(unsigned int value, int start, int count) {
     unsigned int mask = (1 << count) - 1;
     return (value >> start) & mask;
 }
 
 void print_packet(unsigned int packet[])
 {
-    int packet_type = extract_bits(packet[0], 24, 8);  
-    int length = extract_bits(packet[0], 0, 8);       
-    int address = (int) packet[1];          
-    int requester_id = extract_bits(packet[1], 16, 16);
-    int tag = extract_bits(packet[1], 8, 8);          
-    int last_be = extract_bits(packet[2], 28, 4);      
-    int first_be = extract_bits(packet[2], 24, 4);   
+    int packet_type = extract_bits(packet[0], 24, 8);
+    int length = extract_bits(packet[0], 0, 8);
+    int address = packet[1] & 0xFFFFFFFF;
+    int requester_id = extract_bits(packet[1], 0, 16);
+    int tag = extract_bits(packet[1], 16, 8);
+    int last_be = extract_bits(packet[2], 28, 4);
+    int first_be = extract_bits(packet[2], 24, 4);
 
     if (packet_type == 0x40) 
     {
         printf("Packet Type: Write\n");
-    }
-     else 
+    } 
+    else 
     {
         printf("Packet Type: Read\n");
     }
-    
+
     printf("Address: %d\n", address);
     printf("Length: %d\n", length);
     printf("Requester ID: %d\n", requester_id);
     printf("Tag: %d\n", tag);
     printf("Last BE: %d\n", last_be);
     printf("1st BE: %d\n", first_be);
-    
+
     if (packet_type == 0x40) 
     {
         printf("Data: ");
         for (int i = 3; i < 3 + length; i++) 
-        {  
+        {
             int payload_data = (int) packet[i];
             printf("%d ", payload_data);
         }
@@ -65,4 +64,3 @@ void print_packet(unsigned int packet[])
         printf("Data: \n");
     }
 }
-
