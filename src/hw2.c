643a50be4b67d6ea7969cafc453205c529d8e1c3
Ian Kaufman <Kaufmanian49@gmail.com>
2024-10-04 04:01:37 +0000

    Try 2

--- src/hw2.c
+++ src/hw2.c
@@ -18,7 +18,6 @@ unsigned int* create_completion(unsigned int packets[], const char *memory)
 	return NULL;
 }
 
-
 unsigned int extract_bits(unsigned int value, int start, int count) 
 {
     unsigned int mask = (1 << count) - 1;
@@ -27,30 +26,27 @@ unsigned int extract_bits(unsigned int value, int start, int count)
 
 void print_packet(unsigned int packet[])
 {
- 
-    int packet_type = extract_bits(packet[0], 24, 8); 
-    int length = extract_bits(packet[0], 0, 8);      
-    int address = packet[1] & 0xFFFFFFFF;              
-    int requester_id = extract_bits(packet[1], 0, 16); 
-    int tag = extract_bits(packet[1], 16, 8);         
-    int last_be = extract_bits(packet[2], 8, 4);       
-    int first_be = extract_bits(packet[2], 4, 4);     
+    int packet_type = extract_bits(packet[0], 24, 8);
+    int length = extract_bits(packet[0], 0, 8);
+    int address = packet[1] & 0xFFFFFFFF;
+    int requester_id = extract_bits(packet[1], 16, 16);
+    int tag = extract_bits(packet[1], 8, 8);
+    int last_be = extract_bits(packet[2], 28, 4);
+    int first_be = extract_bits(packet[2], 24, 4);
 
-    
-    printf("%d\n", packet_type);   
-    printf("%d\n", address);       
-    printf("%d\n", length);        
-    printf("%d\n", requester_id);  
-    printf("%d\n", tag);           
-    printf("%d\n", last_be);      
-    printf("%d\n", first_be);     
+    printf("%d\n", packet_type);
+    printf("%d\n", address);
+    printf("%d\n", length);
+    printf("%d\n", requester_id);
+    printf("%d\n", tag);
+    printf("%d\n", last_be);
+    printf("%d\n", first_be);
 
-
-    if (packet_type == 0x40) 
-    {  
+    if (packet_type == 0x40)
+    {
         for (int i = 3; i < 6; i++) 
         {
-            int payload_data = (int) packet[i]; 
+            int payload_data = (int) packet[i];
             printf("%d\n", payload_data);
         }
     }
