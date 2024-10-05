4eccf78f04b97546f481ca583fea2a9c4124b452
Ian Kaufman <Kaufmanian49@gmail.com>
2024-10-05 00:39:55 +0000

    prayer

--- src/hw2.c
+++ src/hw2.c
@@ -7,19 +7,22 @@
 
 void print_packet(unsigned int packet[]) 
 {
-    int packet_type = (*(packet) >> 24) & 0xFF;
+    unsigned int packet_type = *packet >> 10;
 
-    if ((packet_type & 0xC0) == 0x40) 
+    if ((packet_type & 0xFFFFF) == 0 && ((packet_type >> 21) & 0x1) == 0) 
     {
-        printf("Packet Type: Write\n");
+        if ((packet_type >> 20) == 1) 
+        {
+            printf("Packet Type: Write\n");
+        } 
+        else 
+        {
+            printf("Packet Type: Read\n");
+        }
     }
-    else if ((packet_type & 0xC0) == 0x00) 
-    {
-        printf("Packet Type: Read\n");
-    } 
     else 
     {
-        printf("Error: Invalid packet type 0x%X\n", packet_type);
+        printf("Error: Invalid packet type\n");
         return;
     }
 
@@ -37,7 +40,7 @@ void print_packet(unsigned int packet[])
     printf("Last BE: %d\n", last_be);
     printf("1st BE: %d\n", first_be);
 
-    if (packet_type == 0x40) 
+    if ((packet_type >> 20) == 1) 
     {
         printf("Data: ");
         for (int i = 0; i < length; i++) 
