7d9260cd427fc7d99de548e40ea66fc90584b00f
Ian Kaufman <Kaufmanian49@gmail.com>
2024-10-05 22:27:47 +0000

    why

--- src/hw2.c
+++ src/hw2.c
@@ -123,11 +123,11 @@ unsigned int* create_completion(unsigned int packets[], const char *memory)
         if ((mem_index + 4) > boundary) 
         {
             unsigned int split_length = (bytes_to_boundary / 4);
-            
+
             unsigned int* new_completion = (unsigned int*)malloc((3 + split_length) * sizeof(unsigned int));
             new_completion[0] = (0x50 << 24) | split_length;
             new_completion[1] = (220 << 16) | (split_length * 4);
-            new_completion[2] = (requester_id << 16) | (tag << 8) | (mem_index & 0x7F);
+            new_completion[2] = (requester_id << 16) | (tag << 8) | ((mem_index & 0x7F) + 4);
 
             for (unsigned int j = 0; j < split_length; j++) 
             {
