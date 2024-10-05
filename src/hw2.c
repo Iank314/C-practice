513f29384fd00c9eb97e5c082f1c1fb48e3a7342
Ian Kaufman <Kaufmanian49@gmail.com>
2024-10-05 22:50:19 +0000

    why

--- src/hw2.c
+++ src/hw2.c
@@ -98,8 +98,7 @@ void store_values(unsigned int packets[], char *memory)
         packet_start += 3 + length;
     }
 }
-unsigned int* create_completion(unsigned int packets[], const char *memory) 
-{
+unsigned int* create_completion(unsigned int packets[], const char *memory) {
     unsigned int address = packets[2] & 0xFFFFFFFC;
     unsigned int length = packets[0] & 0x3FF;
     unsigned int requester_id = packets[1] >> 16;
@@ -109,18 +108,16 @@ unsigned int* create_completion(unsigned int packets[], const char *memory)
     unsigned int* completion = (unsigned int*)malloc((3 + length) * sizeof(unsigned int));
 
     completion[0] = (0xA << 28) | (length);  
-    completion[1] = (0xDC << 16) | (byte_count); 
-    completion[2] = (requester_id << 16) | (tag << 8) | (lower_address & 0x7F);
+    completion[1] = (0xDC << 16) | byte_count; 
+    completion[2] = (requester_id << 16) | (tag << 8) | lower_address;
 
     unsigned int data_offset = 0;
     unsigned int boundary = 0x4000;
     unsigned int bytes_to_boundary = boundary - (address % boundary);
 
-    if (bytes_to_boundary < byte_count) 
-    {
+    if (bytes_to_boundary < byte_count) {
         unsigned int split_length = bytes_to_boundary / 4;
-        for (unsigned int i = 0; i < split_length; i++) 
-        {
+        for (unsigned int i = 0; i < split_length; i++) {
             completion[3 + i] = *(unsigned int*)(memory + address + data_offset);
             data_offset += 4;
         }
@@ -130,16 +127,14 @@ unsigned int* create_completion(unsigned int packets[], const char *memory)
         second_completion[0] = (0xA << 28) | (length - split_length);
         second_completion[1] = (0xDC << 16) | byte_count;
         second_completion[2] = (requester_id << 16) | (tag << 8) | lower_address;
-        for (unsigned int i = 0; i < (length - split_length); i++) 
-        {
+        for (unsigned int i = 0; i < (length - split_length); i++) {
             second_completion[3 + i] = *(unsigned int*)(memory + address + data_offset);
             data_offset += 4;
         }
         return second_completion;
     }
 
-    for (unsigned int i = 0; i < length; i++) 
-    {
+    for (unsigned int i = 0; i < length; i++) {
         completion[3 + i] = *(unsigned int*)(memory + address + data_offset);
         data_offset += 4;
     }
