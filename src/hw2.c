71b2d51f6099ec4a13656d12e903e7233d89b482
Ian Kaufman <Kaufmanian49@gmail.com>
2024-10-05 21:45:47 +0000

    redo

--- src/hw2.c
+++ src/hw2.c
@@ -100,38 +100,23 @@ void store_values(unsigned int packets[], char *memory)
 }
 unsigned int* create_completion(unsigned int packets[], const char *memory) 
 {
-    unsigned int address = packets[2];
+    unsigned int address = packets[2] & 0xFFFFFFFC;
     unsigned int length = packets[0] & 0x3FF;
-    unsigned int requester_id = (packets[1] >> 16) & 0xFFFF;
+    unsigned int requester_id = packets[1] >> 16;
     unsigned int tag = (packets[1] >> 8) & 0xFF;
-
-    unsigned int byte_count = length * 4;
     unsigned int lower_address = address & 0x7F;
 
-    unsigned int *completion_packets = (unsigned int *) malloc(30 * (3 + length) * sizeof(unsigned int));
-    if (!completion_packets) 
+    unsigned int* completion = (unsigned int*)malloc((3 + length) * sizeof(unsigned int));
+    
+    completion[0] = (0x50 << 24) | length;
+    completion[1] = (220 << 16) | (length * 4);
+    completion[2] = (requester_id << 16) | (tag << 8) | lower_address;
+    
+    for (unsigned int i = 0; i < length; i++) 
     {
-        return NULL;
+        unsigned int mem_index = address + i * 4;
+        completion[3 + i] = *(unsigned int *)(memory + mem_index);
     }
-
-    unsigned int current_length = length;
-
-    if ((address & 0x3FFF) + (current_length * 4) > 0x4000) 
-    {
-        current_length = (0x4000 - (address & 0x3FFF)) / 4;
-    }
-
-    unsigned int completion_packet_index = 0;
-
-    completion_packets[completion_packet_index++] = (0xDC << 24) | current_length;
-    completion_packets[completion_packet_index++] = (220 << 24) | (byte_count & 0xFFF);
-    completion_packets[completion_packet_index++] = (requester_id << 16) | (tag << 8) | lower_address;
-
-    for (unsigned int i = 0; i < current_length; i++) 
-    {
-        int mem_index = address + (i * 4);
-        completion_packets[completion_packet_index++] = *((unsigned int *)(memory + mem_index));
-    }
-
-    return completion_packets;
+    
+    return completion;
 }
